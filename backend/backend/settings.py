21d2dc98c42525347da7b33cd156a80263dbaa32
Brandon O'Regan <brandon_oregan97@hotmail.com>
2024-02-07 21:09:59 +0000

    adjust templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-          os.path.join(BASE_DIR, 'frontend/build/static')
+          BASE_DIR / 'frontend' / 'build'
         ],
         "APP_DIRS": True,
         "OPTIONS": {
