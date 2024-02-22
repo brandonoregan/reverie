1dcd90c7058141e429aa41d34b3cdf18b9948265
Brandon O'Regan <brandon_oregan97@hotmail.com>
2024-02-22 13:58:14 +0000

    change static url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -211,7 +211,8 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = 'http://reverie-bucket.s3-website-us-east-1.amazonaws.com/'
+STATIC_URL = 'https://yourbucketname.s3.amazonaws.com/static/'
+
 # STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
